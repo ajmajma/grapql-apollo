4efa51b0819b6303da4fad4ac1aeddaef00ec958
Alex Moser <alex@shopfollain.com>
2017-11-23 07:47:55 -0500

    Sets up mongo + graphql express server to test apollo client

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://alex:pass123@ds119306.mlab.com:19306/graphqllyricapp';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
